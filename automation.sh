2d160056085314c958953d2d9c094625b4c1bd25
Ahmed Adel Shousha <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-09-12 12:05:47 +0300

    automation script for terraform credentials

---
+++ automation.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+echo "
+############################################
+        ___        ____  ____   _    _   ___
+||  || ||    ||   ||    ||  || ||\  /|| ||
+||  || | ==  ||   ||    ||  || || \/ || | ==
+||/\|| ||__  ||__ ||___ ||__|| ||    || ||__
+                    
+############################################
+"
+echo "This script will either"
+echo "1- Run terraform scripts to Provision simple infrastructure In OCI:"
+echo "   - Compartment to contain the resources"
+echo "   - VCN with (IGW, Public Security List, Public subnet) "
+echo "   - Compute Instance"
+echo "2- Destroy the created infrastructure"
+echo "------------------------------------------------------------------------------------------------------------------------------"
+read -p "
+Enter either 1 -> (Create) 
+          or 2 -> (Destroy): " desicion
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+case $desicion in
+        1)
+                echo "----------------------------------"
+                echo "Prerequisites: 1- Tenancy OCID"
+                echo "               2- User OCID"
+                echo "               3- Fingerprint "
+                echo "               4- Region "
+                echo "--------------------------------------------------------------------"
+                echo "PLEASE ENTER YOUR OCI CREDENTIALS"
+                echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                read -p "Enter your Tenancy OCID: " tenancy_ocid
+                read -p "Enter your User OCID: " user_ocid
+                read -p "Enter your Fingerprint: " fingerprint
+                read -p "Enter your Region: " region_identifier
+                read -p "Enter Private Key Path: " rsa_private_key_path
+                export tenancy-ocid=$tenancy_ocid
+                export user-ocid=$user_ocid
+                export fingerprint=$fingerprint
+                export region-identifier=$region_identifier
+                export rsa-private-key-path=$rsa_private_key_path
+                terraform init
+                terraform apply 
+                ;;
+        2)
+                echo "PLEASE ENTER YOUR OCI CREDENTIALS To Delete Infrastructure"
+                echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                read -p "Enter your Tenancy OCID: " tenancy_ocid
+                read -p "Enter your User OCID: " user_ocid
+                read -p "Enter your Fingerprint: " fingerprint
+                read -p "Enter your Region: " region_identifier
+                read -p "Enter Private Key Path:" rsa_private_key_path
+                export tenancy-ocid=$tenancy_ocid
+                export user-ocid=$user_ocid
+                export fingerprint=$fingerprint
+                export region-identifier=$region_identifier
+                export rsa-private-key-path=$rsa_private_key_path
+                terraform destroy 
+                ;;
+        *)
+                echo "PLEASE ENTER A VALID OPTION"
+esac

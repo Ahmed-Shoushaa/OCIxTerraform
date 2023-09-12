2b1db2285fb0ad704a90f8d7b0cc1a62e66b92a7
Ahmed Adel Shousha <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-09-12 19:09:06 +0000

    .

--- 3- Network.tf
+++ 3- Network.tf
@@ -71,6 +71,6 @@ resource "oci_core_subnet" "vcn-public-subnet"{
  
   # Optional
   route_table_id = module.vcn.ig_route_id
-  security_list_ids = [oci_core_security_list.public-security-list.id]
+  security_list_ids = oci_core_security_list.public-security-list.id
   display_name = "public-subnet"
 }

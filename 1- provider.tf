0cab5bb2f7884369f4314a7b2fcb8ef0ca679b64
Ahmed Adel Shousha <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-09-12 19:27:53 +0300

    Change auth method to be through the script

--- 1- provider.tf
+++ 1- provider.tf
@@ -1,7 +1,19 @@
-provider "oci" {
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+      version = "5.12.0"
+    }
+  }
+}
+
+# We Provide the auth variables as ENV variables if we use the automation.sh script 
+# If not un comment the below section and add the data between "" 
+
+/* provider "oci" {
   tenancy_ocid = "<tenancy-ocid>"
   user_ocid = "<user-ocid>" 
   private_key_path = "<rsa-private-key-path>"
   fingerprint = "<fingerprint>"
   region = "<region-identifier>"
-}
+} */

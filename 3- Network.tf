faa38e076069a6e0801063af958c2eb18454d49d
Ahmed Adel Shousha <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-09-12 18:27:55 +0300

    Grouping network resources

---
+++ 3- Network.tf
@@ -0,0 +1,76 @@
+# CREATE NETWORK COMPONENTS REQUIRED FOR A PUBLIC INSTANCE
+
+module "vcn" {
+  source  = "oracle-terraform-modules/vcn/oci"
+  version = "3.5.2"
+  compartment_id = "<compartment id where to create all resources>"
+  create_internet_gateway = true
+  region = "<region to provision infra>"
+  vcn_name = "<required vcn name>"
+  vcn_cidrs = ["10.0.0.0/16"]
+}
+
+resource "oci_core_security_list" "public-security-list"{
+
+# Required
+  compartment_id = "<compartment-ocid>"
+  vcn_id = module.vcn.vcn_id
+
+# Optional
+  display_name = "security-list-for-public-subnet"
+
+# Allows all outgoing traffic (regardless of the protocol) to any destination IP address. 
+  egress_security_rules {
+      stateless = false
+      destination = "0.0.0.0/0"
+      destination_type = "CIDR_BLOCK"
+      protocol = "all" 
+  }
+# Allow ssh connections
+  ingress_security_rules { 
+      stateless = false
+      source = "0.0.0.0/0"
+      source_type = "CIDR_BLOCK"
+      protocol = "6"
+      tcp_options { 
+          min = 22
+          max = 22
+      }
+    }
+# Allow Incoming Ping requests to the Instance
+  ingress_security_rules { 
+      stateless = false
+      source = "0.0.0.0/0"
+      source_type = "CIDR_BLOCK"
+      protocol = "1"
+      icmp_options {
+        type = 3
+        code = 4
+      } 
+    }   
+# Allow HTTP requests to the Instance 
+  ingress_security_rules {
+    stateless = false
+    source = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    protocol = "6"  
+    tcp_options {
+      min = 80  
+      max = 80
+    }
+  }
+
+}
+
+resource "oci_core_subnet" "vcn-public-subnet"{
+
+  # Required
+  compartment_id = "<compartment-ocid>"
+  vcn_id = module.vcn.vcn_id
+  cidr_block = "10.0.0.0/24"
+ 
+  # Optional
+  route_table_id = module.vcn.ig_route_id
+  security_list_ids = [oci_core_security_list.public-security-list.id]
+  display_name = "public-subnet"
+}
--- 3.1- vcn-module.tf
+++
@@ -1,9 +0,0 @@
-module "vcn" {
-  source  = "oracle-terraform-modules/vcn/oci"
-  version = "3.5.2"
-  compartment_id = "<compartment id where to create all resources>"
-  create_internet_gateway = true
-  region = "<region to provision infra>"
-  vcn_name = "<required vcn name>"
-  vcn_cidrs = ["10.0.0.0/16"]
-}

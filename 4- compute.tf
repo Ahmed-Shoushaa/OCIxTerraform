2298f771f7c30838455eebb5ade6f7778fc8d9db
Ahmed Adel Shousha <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-09-12 20:11:12 +0000

    .

--- 4- compute.tf
+++ 4- compute.tf
@@ -9,6 +9,7 @@ resource "oci_core_instance" "Wind_IS" {
     compartment_id = oci_identity_compartment.tf-compartment.compartment_id
     display_name = "Wind_IS"
     shape = "VM.Standard2.1"
+    # choose the suitable id from here based on your region https://docs.oracle.com/en-us/iaas/images/image/98ca8567-3317-4441-82a9-115a79500437/
     source_details {
         source_id = "ocid1.image.oc1.iad.aaaaaaaaevjttsicdlm4h3zomclg6pztgxgg7ba54e27c4oopvkbaftvjqna" 
         source_type = "image"

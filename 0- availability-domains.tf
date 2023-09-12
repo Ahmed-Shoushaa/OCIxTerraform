2a9288964196b58c53e7ad6f7496b3f7d1f5cbc6
Ahmed Adel Shousha <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-09-12 20:25:21 +0300

    adjust vars

--- 0- availability-domains.tf
+++
@@ -1,6 +0,0 @@
-/* fetching the availability domains in the tenancy */
-
-data "oci_identity_availability_domains" "ads" {
-  compartment_id = var.tenancy_ocid
-}
-
--- 0- vars.tf
+++ 0- vars.tf
@@ -3,3 +3,9 @@ variable "user_ocid" {}
 variable "fingerprint" {}
 variable "region" {}
 variable "private_key_path" {}
+
+/* fetching the availability domains in the tenancy */
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.tenancy_ocid
+}
+

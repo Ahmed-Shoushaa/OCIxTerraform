fa068e45678b49a67af6f2465608e1faba17ce41
Ahmed Adel Shousha <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-09-12 20:23:43 +0300

    adjust outputs

--- 5- outputs.tf
+++ 5- outputs.tf
@@ -1,11 +1,11 @@
-# Output the "list" of all availability domains.
-output "all-availability-domains-in-your-tenancy" {
-  value = data.oci_identity_availability_domains.ads.availability_domains
+# Outputs for compute instance
+output "public-ip-for-compute-instance" {
+  value = oci_core_instance.Wind_IS.public_ip
 }
 
-# The "name" of the availability domain to be used for the compute instance.
-output "name-of-first-availability-domain" {
-  value = data.oci_identity_availability_domains.ads.availability_domains[0].name
+/* # Output the "list" of all availability domains.
+output "all-availability-domains-in-your-tenancy" {
+  value = data.oci_identity_availability_domains.ads.availability_domains
 }
 
 # Outputs for compartment
@@ -21,21 +21,6 @@ output "compartment-OCID" {
 output "vcn_id" {
   description = "OCID of the VCN that is created"
   value = module.vcn.vcn_id
-}
-
-output "id-for-route-table-that-includes-the-internet-gateway" {
-  description = "OCID of the internet-route table. This route table has an internet gateway to be used for public subnets"
-  value = module.vcn.ig_route_id
-}
-
-output "nat-gateway-id" {
-  description = "OCID for NAT gateway"
-  value = module.vcn.nat_gateway_id
-}
-
-output "id-for-for-route-table-that-includes-the-nat-gateway" {
-  description = "OCID of the nat-route table - This route table has a nat gateway to be used for private subnets. This route table also has a service gateway."
-  value = module.vcn.nat_route_id
 }
 
 # Outputs for public security list
@@ -54,8 +39,4 @@ output "public-subnet-OCID" {
   value = oci_core_subnet.vcn-public-subnet.id
 }
 
-
-# Outputs for compute instance
-output "public-ip-for-compute-instance" {
-  value = oci_core_instance.ubuntu_instance.public_ip
-}
+ */

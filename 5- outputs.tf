3b17211e1b7971b50e64c5af8f9b7f6aa54f0815
Ahmed Adel Shousha <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-09-12 21:16:37 +0000

    .

--- 5- outputs.tf
+++ 5- outputs.tf
@@ -20,7 +20,7 @@ output "compartment-OCID" {
 # Outputs for the vcn module
 output "vcn_id" {
   description = "OCID of the VCN that is created"
-  value = module.vcn.vcn_id
+  value       = module.vcn.vcn_id
 }
 
 # Outputs for public security list
